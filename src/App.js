8f19910f3daa040cb2a1681a42dee316108f32cf
vishnugopinathv <vishnugopinathv@gmail.com>
2022-05-23 23:59:38 +0530

    assessment4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Home from './pages/Home';
+import Students from './pages/Students';
+import Contact from './pages/Contact';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/students' element={<Students />}/>
+      <Route path='/contact' element={<Contact />}/>
+    </Routes>
+    </BrowserRouter>
+    
+    
   );
 }
 
---
+++ src/pages/Students.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import Layout from './Layout'
+
+function Students() {
+  return (
+    <Layout>
+        <div className="table__container">
+        <div className="student__page__head">
+            <h1>Students details</h1>
+            <button className='add__btn'>Add New Student</button>
+        </div>
+        <table>
+            <tr>
+                <td>Name</td>
+                <td>Age</td>
+                <td>Course</td>
+                <td>Batch</td>
+                <td>Change</td>
+            </tr>
+            <tr>
+                <td>John</td>
+                <td>24</td>
+                <td>MERN</td>
+                <td>October</td>
+                <td><a href="#">Edit</a></td>
+            </tr>
+            <tr>
+                <td>Doe</td>
+                <td>23</td>
+                <td>MERN</td>
+                <td>September</td>
+                <td><a href="#">Edit</a></td>
+            </tr>
+            <tr>
+            <td>Biden</td>
+                <td>25</td>
+                <td>MERN</td>
+                <td>December</td>
+                <td><a href="#">Edit</a></td>
+            </tr>
+            <tr>
+            <td>Doe</td>
+                <td>21</td>
+                <td>MERN</td>
+                <td>February</td>
+                <td><a href="#">Edit</a></td>
+            </tr>
+        </table>
+        </div>
+    </Layout>
+  )
+}
+
+export default Students
