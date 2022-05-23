fe9c00823cb169109e0e0c07c85f42c43ca9d9a6
vishnugopinathv <vishnugopinathv@gmail.com>
2022-05-24 00:05:57 +0530

    assessment4

--- src/pages/Students.js
+++ src/pages/Students.js
@@ -22,28 +22,28 @@ function Students() {
                 <td>24</td>
                 <td>MERN</td>
                 <td>October</td>
-                <td><a href="#">Edit</a></td>
+                <td><a href="/students">Edit</a></td>
             </tr>
             <tr>
                 <td>Doe</td>
                 <td>23</td>
                 <td>MERN</td>
                 <td>September</td>
-                <td><a href="#">Edit</a></td>
+                <td><a href="/students">Edit</a></td>
             </tr>
             <tr>
             <td>Biden</td>
                 <td>25</td>
                 <td>MERN</td>
                 <td>December</td>
-                <td><a href="#">Edit</a></td>
+                <td><a href="/students">Edit</a></td>
             </tr>
             <tr>
             <td>Doe</td>
                 <td>21</td>
                 <td>MERN</td>
                 <td>February</td>
-                <td><a href="#">Edit</a></td>
+                <td><a href="/students">Edit</a></td>
             </tr>
         </table>
         </div>
